1f42ed63626b3ed725df0ff84b155c88b2942f58
Sungguk Lim <limasdf@gmail.com>
2014-10-25 20:49:38 +0900

    Rename nikes.* to index.*

--- app/background.js
+++ app/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('nikes.html', {
+  chrome.app.window.create('index.html', {
     'id': '_mainWindow', 'bounds': {'width': 800, 'height': 600 }
   });
 });

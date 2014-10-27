96b31b7deaee2a06d9b9066d273d604984cf537a
Sungguk Lim <limasdf@gmail.com>
2014-10-27 09:18:58 +0900

    ArraybufferToString

--- app/lib/utils.dart
+++ app/lib/utils.dart
@@ -1,10 +1,13 @@
 library beagle.utils;
 
+import 'dart:convert';
+
 import 'package:chrome/chrome_ext.dart' as chrome;
 
 String ArraybufferToString(chrome.ArrayBuffer buffer) {
-  // TODO(sunglim): Implement. Implement Test.
-  return "TEST";
+  // TODO(sunglim): Write Test. [48, 56, 49, 51]
+  AsciiDecoder ad = new AsciiDecoder();
+  return ad.convert(buffer.getBytes());
 }
 
 chrome.ArrayBuffer StringToArraybuffer(String str) {
